b3345aa515041eab89e55025dd23fa2b563244c8
Sanjar17 <sanjarsaidov173@gmail.com>
2023-08-08 14:17:52 +0500

    static urls

--- TicketBooking/urls.py
+++ TicketBooking/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+# add static files
+from django.conf import settings 
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +30,4 @@ urlpatterns = [
     path('reservations/', include('reservations.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'))
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
